1fce5781960d0ab150f52a4ed83478d754cc4897
Ben Eccles <ben.g.eccles@icloud.com>
2019-09-06 10:19:44 -0600

    added modules to gameCTRL to add a favorite, change a favorite name, send favorites, and remove favorite.

---
+++ server/controllers/gameCTRL.js
@@ -0,0 +1,53 @@
+const collections = [];
+const newFavoriteList = ["New Favorites"];
+let index = 0;
+
+module.exports = {
+    add: (req, res) => {
+        // Get id, title, release date, studio, platforms, background_image,
+        // genres, short_screenshots, clip(maybe), and short_Description, then store in object
+        const favoriteObj = {
+            index: index++
+            id: req.body.id,
+            title: req.body.name,
+            date: req.body.released,
+            studio: req.body.developers,
+            platforms: req.body.platforms,
+            background_image: [req.body.background_image, req.body.background_image_additional],
+            genres: req.body.genres,
+            tags: req.body.tags,
+            clips: "", // For some reason, this only appears on the GET for recommended games and not for the originally searched for game.
+            short_screenshots: [], // Only appears on RAWG recommendations GET
+            description: req.body.description_raw,
+            esrb: req.body.esrb_rating.name
+
+        }
+
+        newFavoriteList.push(favoriteObj)
+        res.status(200).send(newFavoriteList)
+    },
+    rename: (req, res) => {
+        //Rename favorites list
+        const {id} = req.params
+        const {newName} = req.body
+        const index = collections.findIndex(el => el.id === +id)
+        const oldName = collections[index][0].shift()
+       
+        if (newName) {
+            collections[index][0].unshift(newName);
+        } else {
+            collections[index][0].unshift(oldName);
+        }
+
+        res.statusw(200).send(collections);
+    },
+    send: () => {
+        res.status(200).send(collections)
+    },
+    remove: (req, res) => {
+        const {id} = req.params
+        const index = collections.findIndex(el => el.id === +id)
+        collections.splice(index, 1)
+        res.status(200).send(collections)
+    }
+}
--- server/server.js
+++ server/server.js
@@ -6,3 +6,31 @@ const gameCtrl = require('./controllers/gameCTRL')
 const recieveCtrl = require('./controllers/recieveCTRL')
 const recommendCtrl = require('./controllers/recommendCTRL')
 
+// Top level middleware
+app.use(express.json())
+
+/***SERVER ENDPOINTS***/
+//gameCTRL.js
+// GET to gameCTRL.getFavorites
+
+// POST to gameCTRL.addToFavorites
+
+// PUT to gameCTRL.updateName
+
+// DELETE to gameCTRL.removeFavorite
+
+
+//recommendCTRL.js
+// GET to recommendCTRL.recieveRecommendations
+
+// DELETE to recommendCTRL.removeRecommendations
+
+
+//recieveCTRL
+// GET to recieveCTRL.recieveSearchResult
+
+// DELETE to recieveCTRL.removeResults
+
+
+// Listen up, for those sweet, sweet axios calls.
+app.listen(SERVER_PORT, () => console.log(`Port ${SERVER_PORT} is on station!`))
